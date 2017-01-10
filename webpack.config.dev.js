5612e3b5d48f3ea6b5bcdf8e535d37205979dd04
mtcrouse <mettacrouse@gmail.com>
2017-01-09 17:20:06 -0800

    get some package.json things fixed

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -24,7 +24,16 @@ module.exports = {
     loaders: [{
       test: /\.js|\.jsx$/,
       loaders: ['babel-loader'],
-      include: APP_DIR
+      include: path.join(__dirname, 'src')
+    }, {
+      test: /\.json$/,
+      loader: "json-loader"
+    }, {
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      loaders: [
+        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+      ]
     }]
   },
   node: {
