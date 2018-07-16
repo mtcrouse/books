aaa44c4bbe2ab0f7f006d5ae9822b03ffef73db9
mtcrouse <mettacrouse@gmail.com>
2018-07-15 22:35:09 -0700

    add forms to signin page

--- src/client/app/components/SignIn.jsx
+++ src/client/app/components/SignIn.jsx
@@ -4,17 +4,36 @@ class SignIn extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  doSignIn(event) {
+    return;
   }
 
   render() {
     return (
       <div className="container">
         <h3 className="cormorant left-align">Sign In</h3>
-        <p className="left-align">
-          Come on in!
-        </p>
         <div className="row">
-          <a href="/auth/signin"><button>Sign in</button></a>
+          <div className="six columns">
+            <form onSubmit={this.doSignIn}>
+              <input type="text" placeholder="username" name="username" onChange={this.handleChange} />
+              <input type="password" placeholder="password" name="password" onChange={this.handleChange} />
+            </form>
+            <a href="/auth/signin"><button>Sign In</button></a>
+          </div>
+          <div className="six columns">
+            <form onSubmit={this.doSignIn}>
+              <input type="text" placeholder="username" name="username" onChange={this.handleChange} />
+              <input type="password" placeholder="password" name="password" onChange={this.handleChange} />
+            </form>
+            <a href="/auth/signin"><button>Create Account</button></a>
+          </div>
         </div>
       </div>
     );
