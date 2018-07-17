5f26f5190a8e8f1708abf3bfc99cca4cc6127cf1
mtcrouse <mettacrouse@gmail.com>
2018-07-16 17:39:01 -0700

    fix sign-in error

--- routes/auth.js
+++ routes/auth.js
@@ -9,20 +9,19 @@ const request = require('request-promise')
 const router = express.Router();
 
 router.get('/auth/signin', (req, res, next) => {
-  let userId = 1;
-  knex('users')
-    .where('id', userId)
-    .first()
-    .then((row) => {
-      if (!row) {
-        return next(boom.create(400, `No user at id ${userId}`));
-      }
-
-      res.send(camelizeKeys(row));
-    })
-    .catch((err) => {
-      next(err);
-    });
+  // knex('users')
+  //   .where('first_name', 'metta')
+  //   .first()
+  //   .then((row) => {
+  //     if (!row) {
+  //       return next(boom.create(400, `No user at id ${userId}`));
+  //     }
+  //
+  //     res.send(camelizeKeys(row));
+  //   })
+  //   .catch((err) => {
+  //     next(err);
+  //   });
 
   const expiry = new Date(Date.now() + 1000 * 60 * 60 * 24 * 60);
   const token = jwt.sign({ userId: 1 }, process.env.JWT_SECRET, {
